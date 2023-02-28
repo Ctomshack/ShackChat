ee2f49a3d1cea9a8d29f0565d4135b67100a917e
Chris Tomshack <ctomshack15@gmail.com>
2023-02-28 14:31:55 -0500

    firebase.initialize added

--- components/ChatRoom.js
+++ components/ChatRoom.js
@@ -7,6 +7,16 @@ import "firebase/compat/analytics";
 import { useUserContext } from "@/context/userContext";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
+firebase.initializeApp({
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_G_TAG,
+});
+
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
