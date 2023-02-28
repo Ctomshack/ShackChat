13431f13c2152d3990458d86a71133a7cb3bac16
Chris Tomshack <ctomshack15@gmail.com>
2023-02-28 13:45:00 -0500

    Send button styling

--- components/Login.js
+++ components/Login.js
@@ -76,7 +76,7 @@ export default function Login() {
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
         </div>
 
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="mt-2 sm:mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
 
 
