a808f1c6cef79d08f6118ca44e57b6976adb1463
Chris Tomshack <ctomshack15@gmail.com>
2023-02-28 13:16:58 -0500

    bubble dropshadow added

--- pages/index.js
+++ pages/index.js
@@ -136,7 +136,7 @@ function ChatRoom() {
 function ChatMessage(props) {
   const { text, uid, photoURL, displayName } = props.message;
 
-  console.log(props.message)
+  // console.log(props.message)
 
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
@@ -145,7 +145,7 @@ function ChatMessage(props) {
       <div className={` ${messageClass} w-full message flex-row gap-0.5`}>
 
       <div className={`text-sm text-gray-600 self-middle ${messageClass} px-2`}>{displayName}</div>
-      <p className=''>{text}</p>
+      <p className='dropshadow-lg shadow-md'>{text}</p>
       </div>
 
     </div>
